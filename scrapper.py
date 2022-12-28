2ff336fbbe050dcd5c35ec780dd849575f1795f4
LucasManchadoMarcos <lucas.manchado@estudiantat.upc.edu>
2022-12-28 12:09:29 +0100

    added subs

--- scrapper.py
+++ scrapper.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 reddit = praw.Reddit(client_id='oipIm2GxfZBd77OBwOPyvA', client_secret='DYC59MfqbuAhpwIpgZGx_IBDnk99zA', user_agent='BDEproj')
 
-hot_posts = reddit.subreddit('MachineLearning').top(time_filter= "week", limit=100)
+hot_posts = reddit.subreddit('MachineLearning+AskHistorians+philosophy').top(time_filter= "week", limit=1000)
 posts = []
 
 for post in hot_posts:
+    sub = post.subreddit.display_name
     title = post.title
     title = title.replace(",", "")
     desc = post.selftext
@@ -14,7 +15,7 @@ for post in hot_posts:
     text = title + ' ' + desc
     link = post.permalink
     id = post.id
-    posts.append([title, text, link, id])
+    posts.append([sub, title, text, link, id])
 
-posts = pd.DataFrame(posts,columns=['title', 'text', 'link', 'id'])
+posts = pd.DataFrame(posts,columns=['subreddit', 'title', 'text', 'link', 'id'])
 posts.to_csv('posts.csv', index = False)

625f4558ddbef8c21f4da5a54c6107e363628d7f
LucasManchadoMarcos <lucas.manchado@estudiantat.upc.edu>
2022-12-29 11:30:35 +0100

    Restarting again

--- oozie/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python scrapper.py
-hdfs dfs -moveFromLocal HELL.txt hdfs://au/user/pk.stradomski-ece/oozie/
--- oozie/scripts/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "foo" > HELL.txt
-hdfs dfs -moveFromLocal HELL.txt hdfs://au/user/pk.stradomski-ece/oozie/
